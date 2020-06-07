178afc18fa3ca34108d7ff3b008e9674ed1d547d
Filiipe <filipessm124@gmail.com>
2020-06-07 22:18:06 +0200

    Finish web config

---
+++ web/src/routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import CreatePoint from './pages/CreatePoint';
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Route component={Landing} path="/" exact />
+      <Route component={CreatePoint} path="/create-point" exact />
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
